e33cfab79d64dd217aae682a0d26bca72d709b45
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-07 18:53:48 +0000

    some js edits mostly deleted semicolons

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 alert('Hello, world!')
 
 function backgroundyellow () { // eslint-disable-line no-unused-vars
-  document.getElementById("paragraph").style.backgroundColor = "yellow";
+  document.getElementById('paragraph').style.backgroundColor = 'yellow'
 }
 
-function switchtext() {
-  var x = document.getElementById("myDIV");
+function switchtext () {
+  var x = document.getElementById("myDIV")
   if (x.innerHTML === "Hello") {
-    x.innerHTML = "Goodbye";
+    x.innerHTML = "Goodbye"
   } else {
-    x.innerHTML = "Hello";
+    x.innerHTML = "Hello"
   }
 }
 
-function HidingText() {
-  var x = document.getElementById("myDIV");
+function HidingText () {
+  var x = document.getElementById("myDIV")
   if (x.style.display === "none") {
-    x.style.display = "block";
+    x.style.display = "block"
   } else {
-    x.style.display = "none";
+    x.style.display = "none"
   }
 }
