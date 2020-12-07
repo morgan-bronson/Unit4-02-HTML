278de75212f047c76ff56b17118b8d65930dcabf
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-07 17:23:14 +0000

    background colour

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict'
+alert('Hello, world!')
+
+function backgroundyellow () { // eslint-disable-line no-unused-vars
+  document.getElementById("paragraph").style.backgroundColor = "yellow";
+}
