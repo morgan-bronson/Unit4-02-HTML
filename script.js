5bbade512cd155b69dbaf008ad2e17150fc61dbe
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-08 14:55:52 +0000

    all working

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ function backgroundyellow () { // eslint-disable-line no-unused-vars
   document.getElementById('paragraph').style.backgroundColor = 'yellow'
 }
 
-function switchtext () {
-  let x = document.getElementById('myDIV')
-  if (x.innerHTML === 'Hello') {
-    x.innerHTML = 'Goodbye'
-  } else {
-    x.innerHTML = 'Hello'
-  }
-}
-
 function HidingText () {
-  let x = document.getElementById('myDIV')
+  let x = document.getElementById('mydiv')
   if (x.style.display === 'none') {
     x.style.display = 'block'
   } else {
     x.style.display = 'none'
+  }
+}
+
+function switchtext () {
+  let x = document.getElementById('mydiv1')
+  if (x.innerHTML === 'Hello') {
+    x.innerHTML = 'Goodbye'
+  } else {
+    x.innerHTML = 'Hello'
   }
 }
